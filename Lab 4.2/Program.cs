9970114662cdb066ad55c1149d0f6c457129cc56
krist <krist@LAPTOP-HIQLHLL6>
2022-11-02 20:23:20 +0500

    Добавьте файлы проекта.

---
+++ Lab 4.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumPolozhit = 0;
+            int sumOtric = 0;
+            int i = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                int i1 = Convert.ToInt32(Console.ReadLine());
+                if (i1 > 0)
+                    sumPolozhit++;
+                if (i1 < 0)
+                    sumOtric++;
+                i = i1;
+            }
+            while ( i != 0);
+            if (sumPolozhit > sumOtric)
+                Console.WriteLine("больше положительных ");
+            if (sumPolozhit < sumOtric)
+                Console.WriteLine("больше отрицательных ");
+            if (sumPolozhit == sumOtric)
+                Console.WriteLine("одинаково ");
+        }
+    }
+}
